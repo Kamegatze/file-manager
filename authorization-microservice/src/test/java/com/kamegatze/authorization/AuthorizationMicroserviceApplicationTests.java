9feee12c3a63eb680fda606bf82e8926746aab9b
Shirayev Aleksey <aleksi.aleksi2014@yandex.ru>
2023-12-09 15:06:15 +0300

    init

---
+++ authorization-microservice/src/test/java/com/kamegatze/authorization/AuthorizationMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamegatze.authorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
