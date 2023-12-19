6fa4f1c417a739a2a3e9085a5b3b0dfcd3a55e03
Shirayev Aleksey <aleksi.aleksi2014@yandex.ru>
2023-12-19 19:11:33 +0300

    Исправление импорта

--- authorization-microservice/src/main/java/com/kamegatze/authorization/controllers/AuthenticationController.java
+++ authorization-microservice/src/main/java/com/kamegatze/authorization/controllers/AuthenticationController.java
@@ -4,15 +4,10 @@ import com.kamegatze.authorization.dto.JwtDto;
 import com.kamegatze.authorization.dto.Login;
 import com.kamegatze.authorization.dto.Response;
 import com.kamegatze.authorization.dto.UsersDto;
-import com.kamegatze.authorization.exception.RefreshTokenIsNullException;
-import com.kamegatze.authorization.exception.UserNotExistException;
 import com.kamegatze.authorization.exception.UsersExistException;
 import com.kamegatze.authorization.service.AuthorizationService;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.io.IOException;
+
 import java.util.Map;
 
 @RestController
--- authorization-microservice/src/main/java/com/kamegatze/authorization/service/AuthorizationService.java
+++ authorization-microservice/src/main/java/com/kamegatze/authorization/service/AuthorizationService.java
@@ -3,11 +3,7 @@ package com.kamegatze.authorization.service;
 import com.kamegatze.authorization.dto.JwtDto;
 import com.kamegatze.authorization.dto.Login;
 import com.kamegatze.authorization.dto.UsersDto;
-import com.kamegatze.authorization.exception.RefreshTokenIsNullException;
-import com.kamegatze.authorization.exception.UserNotExistException;
 import com.kamegatze.authorization.exception.UsersExistException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
