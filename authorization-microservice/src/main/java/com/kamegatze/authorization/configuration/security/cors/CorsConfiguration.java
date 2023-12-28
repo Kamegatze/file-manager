738dd11ffb229aa52746658b8e831c5bde576549
Shirayev Aleksey <aleksi.aleksi2014@yandex.ru>
2023-12-28 23:54:07 +0300

    Добавление cors конфигурации

---
+++ authorization-microservice/src/main/java/com/kamegatze/authorization/configuration/security/cors/CorsConfiguration.java
@@ -0,0 +1,36 @@
+package com.kamegatze.authorization.configuration.security.cors;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration {
+    @Bean
+    public WebMvcConfigurer corsConfig() {
+
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NotNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowCredentials(true)
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedHeaders(
+                                HttpHeaders.CONTENT_TYPE,
+                                HttpHeaders.ACCEPT
+                        )
+                        .allowedMethods(
+                                HttpMethod.GET.name(),
+                                HttpMethod.POST.name(),
+                                HttpMethod.PUT.name(),
+                                HttpMethod.DELETE.name()
+                        )
+                        .maxAge(3600L);
+            }
+        };
+    }
+}
