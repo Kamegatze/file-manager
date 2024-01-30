5330cbbd06f819d986a25ab0fda8815e1ed9e7a3
Shirayev Aleksey <aleksi.aleksi2014@yandex.ru>
2024-01-31 02:04:15 +0300

    Изменение супер класса в UserNotExistException

--- authorization-microservice/src/main/java/com/kamegatze/authorization/exception/UserNotExistException.java
+++ authorization-microservice/src/main/java/com/kamegatze/authorization/exception/UserNotExistException.java
@@ -1,6 +1,6 @@
 package com.kamegatze.authorization.exception;
 
-public class UserNotExistException extends Exception {
+public class UserNotExistException extends RuntimeException {
     public UserNotExistException(String message) {
         super(message);
     }
