97b08e0677859ff80764ec8b4d92347d231ad70f
Shirayev Aleksey <aleksi.aleksi2014@yandex.ru>
2024-01-31 01:41:56 +0300

    Исправление ошибки валидации в dto

--- authorization-microservice/src/main/java/com/kamegatze/authorization/dto/ChangePasswordDto.java
+++ authorization-microservice/src/main/java/com/kamegatze/authorization/dto/ChangePasswordDto.java
@@ -2,15 +2,12 @@ package com.kamegatze.authorization.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Data;
 import lombok.ToString;
-
-
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
-
 @Data
 @Builder
 @ToString
--- authorization-microservice/src/main/java/com/kamegatze/authorization/dto/Login.java
+++ authorization-microservice/src/main/java/com/kamegatze/authorization/dto/Login.java
@@ -2,13 +2,13 @@ package com.kamegatze.authorization.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 @Data
 @Builder
