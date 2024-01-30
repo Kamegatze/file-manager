fa98f93399ff1fc10b77273919c0122bcb3e695c
Shirayev Aleksey <aleksi.aleksi2014@yandex.ru>
2024-01-31 01:45:13 +0300

    Локализация exception на английском

--- authorization-microservice/src/main/java/com/kamegatze/authorization/configuration/LocaleConfiguration.java
+++ authorization-microservice/src/main/java/com/kamegatze/authorization/configuration/LocaleConfiguration.java
@@ -1,4 +1,21 @@
 package com.kamegatze.authorization.configuration;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+import java.util.Locale;
+
+@Configuration
 public class LocaleConfiguration {
+
+    @Bean
+    public MessageSource messageSource() {
+        Locale.setDefault(Locale.ENGLISH);
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.addBasenames("classpath:security/messages");
+        return messageSource;
+    }
+
 }
