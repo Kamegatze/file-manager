9f4cf19851d507db258b0ef345eb85ad9ff128d4
Aleksey Shirayev <aleksi.aleksi2014@yandex.ru>
2024-03-14 23:36:00 +0300

    Добавление валидации для сервиса FileSystemService

--- file_manager/src/main/java/com/kamegatze/file/manager/dto/filesystem/FolderDto.java
+++ file_manager/src/main/java/com/kamegatze/file/manager/dto/filesystem/FolderDto.java
@@ -1,5 +1,8 @@
 package com.kamegatze.file.manager.dto.filesystem;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,6 +19,10 @@ import java.util.UUID;
 @AllArgsConstructor
 public class FolderDto {
     private UUID id;
+    @NotNull
+    @NotEmpty
+    @NotBlank
     private String name;
+    @NotNull
     private UUID parentId;
 }
--- file_manager/src/main/java/com/kamegatze/file/manager/service/FileSystemService.java
+++ file_manager/src/main/java/com/kamegatze/file/manager/service/FileSystemService.java
@@ -5,16 +5,23 @@ import com.kamegatze.file.manager.dto.filesystem.FileSystemDto;
 import com.kamegatze.file.manager.dto.filesystem.FolderDto;
 import com.kamegatze.file.manager.models.FileSystem;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.validation.annotation.Validated;
 
 import java.sql.Blob;
 import java.util.List;
 import java.util.UUID;
 
+@Validated
 public interface FileSystemService {
-    FileSystemDto createFolderByFolderParentId(FolderDto fileSystemDto, HttpServletRequest httpServletRequest);
-    FileSystemDto createSaveFileByFolderParentId(FileDto fileSystemDto, HttpServletRequest httpServletRequest);
-    List<FileSystemDto> getChildrenByParentId(UUID parentId, HttpServletRequest httpServletRequest);
-    FileSystemDto getFileSystem(UUID fileSystemId);
-    FileSystem getFileByFileId(UUID fileId);
-    UUID deleteFileSystemById(UUID fileSystemId);
+    FileSystemDto createFolderByFolderParentId(@Valid FolderDto fileSystemDto,
+                                               HttpServletRequest httpServletRequest);
+    FileSystemDto createSaveFileByFolderParentId(@Valid FileDto fileSystemDto,
+                                                 HttpServletRequest httpServletRequest);
+    List<FileSystemDto> getChildrenByParentId(@NotNull UUID parentId,
+                                              HttpServletRequest httpServletRequest);
+    FileSystemDto getFileSystem(@NotNull UUID fileSystemId);
+    FileSystem getFileByFileId(@NotNull UUID fileId);
+    UUID deleteFileSystemById(@NotNull UUID fileSystemId);
 }
