999276d1b4323a207316609c883eeca1cd0b62f9
Aleksey Shirayev <aleksi.aleksi2014@yandex.ru>
2024-03-14 23:43:48 +0300

    Добавление валидации для UsersService

--- file_manager/src/main/java/com/kamegatze/file/manager/service/UsersService.java
+++ file_manager/src/main/java/com/kamegatze/file/manager/service/UsersService.java
@@ -1,14 +1,24 @@
 package com.kamegatze.file.manager.service;
 
 import com.kamegatze.file.manager.models.Users;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.validation.annotation.Validated;
 
+@Validated
 public interface UsersService {
 
     @KafkaListener(groupId = "${spring.kafka.consumer.group-id}",
             topics = "${spring.kafka.topics.save.users}")
     void saveUsersFromKafka(ConsumerRecord<String, String> record);
 
-    Users getUsersByLogin(String login);
+    Users getUsersByLogin(@NotNull
+                          @NotBlank
+                          @NotEmpty
+                          @Size(min = 5, message = "Your login need more 5 sign")
+                          String login);
 }
