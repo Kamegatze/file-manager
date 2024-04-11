7f18442bc6b75109361b986def706da17168db83
Aleksey Shirayev <aleksi.aleksi2014@yandex.ru>
2024-04-11 16:58:07 +0300

    addition cors configuration

---
+++ file_manager/src/main/java/com/kamegatze/file/manager/configuration/security/cors/CorsConfiguration.java
@@ -0,0 +1,37 @@
+package com.kamegatze.file.manager.configuration.security.cors;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration {
+    @Bean
+    public WebMvcConfigurer corsConfig() {
+
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings( CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowCredentials(true)
+                        .allowedOrigins("http://localhost:4200/")
+                        .allowedHeaders(
+                                HttpHeaders.CONTENT_TYPE,
+                                HttpHeaders.ACCEPT,
+                                HttpHeaders.AUTHORIZATION
+                        )
+                        .allowedMethods(
+                                HttpMethod.GET.name(),
+                                HttpMethod.POST.name(),
+                                HttpMethod.PUT.name(),
+                                HttpMethod.DELETE.name()
+                        )
+                        .maxAge(3600L);
+            }
+        };
+    }
+}
