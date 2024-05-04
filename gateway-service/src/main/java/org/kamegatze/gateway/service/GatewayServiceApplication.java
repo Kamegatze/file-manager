771da27bc5a8905566b984c48ebf945850414fdf
Aleksey Shirayev <aleksi.aleksi2014@yandex.ru>
2024-05-05 01:40:41 +0300

    addition eureka and gatway-service

--- gateway-service/src/main/java/org/kamegatze/gateway/service/GatewayServiceApplication.java
+++ gateway-service/src/main/java/org/kamegatze/gateway/service/GatewayServiceApplication.java
@@ -1,26 +1,24 @@
-package org.kamegatze.eureka.server;
+package org.kamegatze.gateway.service;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableEurekaServer
 @SpringBootApplication(exclude = {
 		DataSourceAutoConfiguration.class,
 		DataSourceTransactionManagerAutoConfiguration.class,
 		HibernateJpaAutoConfiguration.class,
-		SecurityAutoConfiguration.class,
-		ManagementWebSecurityAutoConfiguration.class
+		SecurityAutoConfiguration.class
 })
-public class EurekaServerApplication {
+@EnableDiscoveryClient
+public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaServerApplication.class, args);
+		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
 
 }
