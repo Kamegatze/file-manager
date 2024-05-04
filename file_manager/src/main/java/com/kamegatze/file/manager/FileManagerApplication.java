4b38d170dabec37a2257da901c99f296069407c2
Shirayev Aleksey <aleksi.aleksi2014@yandex.ru>
2024-05-05 00:43:58 +0300

    addition change

--- file_manager/src/main/java/com/kamegatze/file/manager/FileManagerApplication.java
+++ file_manager/src/main/java/com/kamegatze/file/manager/FileManagerApplication.java
@@ -1,13 +1,18 @@
 package com.kamegatze.file.manager;
 
+import com.kamegatze.file.manager.configuration.security.authentication.jwt.converter.JwtRemoteConverter;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FileManagerApplication {
+public class FileManagerApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FileManagerApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+	}
 }
--- file_manager/src/main/java/com/kamegatze/file/manager/repositories/FileSystemRepository.java
+++ file_manager/src/main/java/com/kamegatze/file/manager/repositories/FileSystemRepository.java
@@ -3,7 +3,9 @@ package com.kamegatze.file.manager.repositories;
 import com.kamegatze.file.manager.models.FileSystem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface FileSystemRepository extends JpaRepository<FileSystem, UUID> {
+    List<FileSystem> findAllByNameAndUserId(String name, UUID user_id);
 }
